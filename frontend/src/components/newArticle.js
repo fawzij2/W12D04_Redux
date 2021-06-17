9988244672f75d18ec65e17e952a1f0394bd0d97
fawzij2 <fawzi.dahamsheh@gmail.com>
2021-06-17 23:14:19 +0300

    ticket 7 CreateNewArticle

--- frontend/src/components/newArticle.js
+++ frontend/src/components/newArticle.js
@@ -1,13 +1,61 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { NewArticleContext } from './../context/newArticle';
+import axios from 'axios';
+import { addArticle } from '../reducer/article';
+import { useDispatch, useSelector } from 'react-redux';
+import jwt from "jsonwebtoken";
 
 const NewArticle = () => {
 	const newArticleContext = useContext(NewArticleContext);
 
-	const handleSubmit = (e) => {
+	const dispatch = useDispatch()
+	const state = useSelector((state) => {
+		return{
+			articles: state.articles.article,
+			login: state.login.token
+		}
+	})
+
+	const [title, setTitle] = useState('');
+	const [description, setDescription] = useState('');
+	const [message, setMessage] = useState('');
+
+	const handleSubmit = async (e) => {
 		e.preventDefault();
-		newArticleContext.createNewArticle();
+		await createNewArticle();
 	};
+
+	function validateToken() {
+		const user = jwt.decode(state.login);
+		if (!user) throw new Error();
+	}
+
+	async function createNewArticle() {
+		try {
+			validateToken();
+			const newArticle = await axios.post(
+				'http://localhost:5000/articles',
+				{
+					title,
+					description,
+				},
+				{
+					headers: {
+						Authorization: `Bearer ${state.login}`,
+					},
+				},
+			);
+
+			dispatch(addArticle(newArticle))
+
+			setMessage('The article has been created successfully');
+		} catch (error) {
+			console.log(error);
+			setMessage(
+				'Error happened while creating a new article, please try again',
+			);
+		}
+	}
 
 	return (
 		<>
@@ -15,16 +63,16 @@ const NewArticle = () => {
 				<input
 					type="text"
 					placeholder="article title here"
-					onChange={(e) => newArticleContext.setTitle(e.target.value)}
+					onChange={(e) => setTitle(e.target.value)}
 				/>
 				<textarea
 					placeholder="article description here"
-					onChange={(e) => newArticleContext.setDescription(e.target.value)}
+					onChange={(e) => setDescription(e.target.value)}
 				></textarea>
 				<button>Create New Article</button>
 			</form>
 
-			{newArticleContext.message && <div>{newArticleContext.message}</div>}
+			{message && <div>{message}</div>}
 		</>
 	);
 };
